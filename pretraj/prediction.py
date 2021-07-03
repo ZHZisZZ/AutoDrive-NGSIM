38bbbfe74821570417874a237bd4a3d8bdfe0747
ZHZisZZ <zhouzhanhui1999@sjtu.edu.cn>
2021-07-04 01:58:37 +0800

    temp save

--- pretraj/prediction.py
+++ pretraj/prediction.py
@@ -1,7 +1,7 @@
 import json
 from os import name
 import numpy as np
-from collections import namedtuple
+from sklearn.linear_model import LinearRegression
 from pretraj.vehicle import Vehicle
 
 from pretraj import *
@@ -14,6 +14,7 @@ def IDM_simulation(
 ) -> np.ndarray:
   pass
 
+
 # def adapt_simulation(
 #     ego: Vehicle, 
 #     pre: Vehicle, 
@@ -21,31 +22,47 @@ def IDM_simulation(
 #     predict_frames: int,
 # ) -> np.ndarray:
 #   # linear regressor
-#   dv = torch.tensor((pre.vel_vector - ego.vel_vector)[:observe_frames])
-#   gt = torch.tensor((ego.space_headway_vector - pre.vehicle_length)[:observe_frames])
-#   a_tru = torch.tensor(ego.acc_vector[:observe_frames])
+#   dv = (pre.vel_vector - ego.vel_vector)[:observe_frames]
+#   gt = (ego.space_headway_vector - pre.vehicle_length)[:observe_frames]
+#   Y = ego.acc_vector[:observe_frames] # acceleration
 
-#   kv = torch.tensor(0.0, requires_grad=True)
-#   kg = torch.tensor(0.0, requires_grad=True)
-#   gs = torch.tensor(0.0, requires_grad=True)
-
-#   lr = 1e-4
-#   epoch = int(1e4)
-#   opt = torch.optim.SGD([kv,kg,gs], lr=lr)
-
-#   for _ in range(epoch):
-#     # pre_a = kv*dv + kg*(gt - gs)
-#     pre_a = kv*dv + kg*(gt - gs)
-#     loss = torch.sum((a_tru - pre_a)**2)
-#     # print(a_tru)
-#     print(loss)
-#     loss.backward()
-#     opt.step()
-#     opt.zero_grad()
+#   X = np.vstack([dv, gt, np.ones(observe_frames)])
+#   c = Y @ X.T @ np.linalg.inv(X @ X.T)
+#   kv = c[0]; kg = c[1]; gs = -c[2]/kg
 
 #   print(kv, kg, gs)
-#   print(a_tru)
-#   print(kv*dv + kg*(gt - gs))
+
+#   # simulation
+#   class State(object):
+#     def __init__(self, a, v, ds):
+#       self.a = a; self.v = v; self.ds = ds
+
+#   pre_states = [State(
+#       pre.acc_vector[i], 
+#       pre.vel_vector[i], 
+#       None) 
+#       for i in range(observe_frames, observe_frames+predict_frames)]
+
+#   ego_state = State(
+#       ego.acc_vector[observe_frames-1], 
+#       ego.vel_vector[observe_frames-1], 
+#       ego.space_headway_vector[observe_frames-1])
+
+#   ds_record = []
+#   dt = .1
+#   for pre_state in pre_states:
+#     dv = pre_state.v - ego_state.v
+#     da = pre_state.a - ego_state.a
+
+#     ego_state.ds += dv * dt + .5 * da * dt**2
+#     ego_state.v += (ego_state.a) * dt
+
+#     gt = ego_state.ds - pre.vehicle_length
+#     ego_state.a = kv * dv + kg * (gt - gs) # different for different models
+#     ds_record.append(ego_state.ds)
+
+#   print(ds_record)
+#   print(ego.space_headway_vector[observe_frames:observe_frames+predict_frames])
 
 def adapt_simulation(
     ego: Vehicle, 
@@ -58,9 +75,13 @@ def adapt_simulation(
   gt = (ego.space_headway_vector - pre.vehicle_length)[:observe_frames]
   Y = ego.acc_vector[:observe_frames] # acceleration
 
-  X = np.vstack([dv, gt, np.ones(observe_frames)])
-  c = Y @ X.T @ np.linalg.inv(X @ X.T)
-  kv = c[0]; kg = c[1]; gs = -c[2]/kg
+  # X = np.vstack([dv, gt, np.ones(observe_frames)])
+  # c = Y @ X.T @ np.linalg.inv(X @ X.T)
+  # kv = c[0]; kg = c[1]; gs = -c[2]/kg
+  X = np.vstack([dv, gt]).T.reshape(-1,2)
+  reg = LinearRegression(positive=True).fit(X, Y)
+
+  kv, kg = reg.coef_; gs = -reg.intercept_/kg
 
   print(kv, kg, gs)
 
@@ -88,9 +109,6 @@ def adapt_simulation(
 
     ego_state.ds += dv * dt + .5 * da * dt**2
     ego_state.v += (ego_state.a) * dt
-    # print(ego_state.a)
-    # print(ego_state.v)
-    # print(pre_state.v)
 
     gt = ego_state.ds - pre.vehicle_length
     ego_state.a = kv * dv + kg * (gt - gs) # different for different models
@@ -117,10 +135,10 @@ if __name__ == '__main__':
     l = json.load(fp)
 
   # 3, fail on 1e-4
-  ego = Vehicle(**l[127]['ego'])
-  pre = Vehicle(**l[127]['pre'])
+  ego = Vehicle(**l[2]['ego'])
+  pre = Vehicle(**l[2]['pre'])
 
   # print(ego.vel_vector)
   # print(pre.vel_vector)
 
-  adapt_simulation(ego, pre, 45, 3)
+  adapt_simulation(ego, pre, 30, 18)
