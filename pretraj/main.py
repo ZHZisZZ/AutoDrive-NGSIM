4748d074bbf6c4f36ca94c4da1d005f1a82a2612
ZHZisZZ <zhouzhanhui1999@sjtu.edu.cn>
2021-07-04 15:44:38 +0800

    temp save

--- pretraj/main.py
+++ pretraj/main.py
@@ -18,7 +18,6 @@ metrics = ADE
 error_IDM = 0; hard_braking_IDM = 0
 error_adapt = 0; hard_braking_adpat = 0
 
-
 for i in range(number_vehicles):
   ego = Vehicle(**pair_info[i]['ego'])
   pre = Vehicle(**pair_info[i]['pre'])
@@ -26,12 +25,13 @@ for i in range(number_vehicles):
   groundtruth_record = ego.space_headway_vector[observe_frames:observe_frames+predict_frames]
 
   ds_record, _, _, hard_braking = predict.predict(ego, pre, observe_frames, predict_frames, 'IDM')
-  error_IDM += ADE(np.array(ds_record), np.array(groundtruth_record))
+  error_IDM += metrics(np.array(ds_record), np.array(groundtruth_record))
   hard_braking_IDM += hard_braking
 
   ds_record, _, _, hard_braking = predict.predict(ego, pre, observe_frames, predict_frames, 'adapt')
-  error_adapt += ADE(np.array(ds_record), np.array(groundtruth_record))
+  error_adapt += metrics(np.array(ds_record), np.array(groundtruth_record))
   hard_braking_adpat += hard_braking
+
 
 error_IDM /= number_vehicles
 error_adapt /= number_vehicles
