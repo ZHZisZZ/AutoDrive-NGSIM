38a59129ae3257877c2375bbdc6e2ba643652d0c
ZHZisZZ <zhouzhanhui1999@sjtu.edu.cn>
2021-07-04 11:26:21 +0800

    temp save

--- pretraj/prediction.py
+++ pretraj/prediction.py
@@ -1,10 +1,44 @@
 import json
-from os import name
 import numpy as np
+from typing import List, Callable
 from sklearn.linear_model import LinearRegression
-from pretraj.vehicle import Vehicle
+from pretraj.vehicle import Vehicle, State
 
 from pretraj import *
+
+
+def simulate(
+    ego_state: State, 
+    pre_states: List[State],
+    control_law: Callable[[State, State], float]
+) -> List[int]:
+  """Simulate the trajectory of ego vehicle according to states and control law.
+
+  Args:
+    ego_state: the state of ego vehicle at the last observed frame.
+    pre_states: the states of precede vehicles over all predicted frames.
+    control_law: a function that maps current states of two vehicles to the
+      acceleration of the ego vehicle.
+
+  Returns:
+    A list of predicted space headway of the ego car, which serve as 
+      trajectory info.
+  """
+  ds_record = []
+  dt = .1
+  for pre_state in pre_states:
+    dv = pre_state.v - ego_state.v
+    da = pre_state.a - ego_state.a
+
+    ego_state.ds += dv * dt + .5 * da * dt**2
+    ego_state.v += (ego_state.a) * dt
+
+    # control_law is different for different models
+    ego_state.a = control_law(ego_state, pre_state)
+    ds_record.append(ego_state.ds)
+
+  return ds_record
+
 
 def IDM_simulation(
     ego: Vehicle, 
@@ -15,119 +49,56 @@ def IDM_simulation(
   pass
 
 
-# def adapt_simulation(
-#     ego: Vehicle, 
-#     pre: Vehicle, 
-#     observe_frames: int, 
-#     predict_frames: int,
-# ) -> np.ndarray:
-#   # linear regressor
-#   dv = (pre.vel_vector - ego.vel_vector)[:observe_frames]
-#   gt = (ego.space_headway_vector - pre.vehicle_length)[:observe_frames]
-#   Y = ego.acc_vector[:observe_frames] # acceleration
-
-#   X = np.vstack([dv, gt, np.ones(observe_frames)])
-#   c = Y @ X.T @ np.linalg.inv(X @ X.T)
-#   kv = c[0]; kg = c[1]; gs = -c[2]/kg
-
-#   print(kv, kg, gs)
-
-#   # simulation
-#   class State(object):
-#     def __init__(self, a, v, ds):
-#       self.a = a; self.v = v; self.ds = ds
-
-#   pre_states = [State(
-#       pre.acc_vector[i], 
-#       pre.vel_vector[i], 
-#       None) 
-#       for i in range(observe_frames, observe_frames+predict_frames)]
-
-#   ego_state = State(
-#       ego.acc_vector[observe_frames-1], 
-#       ego.vel_vector[observe_frames-1], 
-#       ego.space_headway_vector[observe_frames-1])
-
-#   ds_record = []
-#   dt = .1
-#   for pre_state in pre_states:
-#     dv = pre_state.v - ego_state.v
-#     da = pre_state.a - ego_state.a
-
-#     ego_state.ds += dv * dt + .5 * da * dt**2
-#     ego_state.v += (ego_state.a) * dt
-
-#     gt = ego_state.ds - pre.vehicle_length
-#     ego_state.a = kv * dv + kg * (gt - gs) # different for different models
-#     ds_record.append(ego_state.ds)
-
-#   print(ds_record)
-#   print(ego.space_headway_vector[observe_frames:observe_frames+predict_frames])
-
 def adapt_simulation(
-    ego: Vehicle, 
-    pre: Vehicle, 
-    observe_frames: int, 
+    ego: Vehicle,
+    pre: Vehicle,
+    observe_frames: int,
     predict_frames: int,
 ) -> np.ndarray:
-  # linear regressor
+  """Simulate the trajectory of ego vehicle based on adaptation model.
+    
+  """
+  # Linear regressor to learn coefficients kv, kg, gs
   dv = (pre.vel_vector - ego.vel_vector)[:observe_frames]
   gt = (ego.space_headway_vector - pre.vehicle_length)[:observe_frames]
-  Y = ego.acc_vector[:observe_frames] # acceleration
+  Y = ego.acc_vector[:observe_frames] # ground truth acceleration
 
-  # X = np.vstack([dv, gt, np.ones(observe_frames)])
-  # c = Y @ X.T @ np.linalg.inv(X @ X.T)
-  # kv = c[0]; kg = c[1]; gs = -c[2]/kg
   X = np.vstack([dv, gt]).T.reshape(-1,2)
   reg = LinearRegression(positive=True).fit(X, Y)
-
   kv, kg = reg.coef_; gs = -reg.intercept_/kg
 
   print(kv, kg, gs)
 
-  # simulation
-  class State(object):
-    def __init__(self, a, v, ds):
-      self.a = a; self.v = v; self.ds = ds
-
-  pre_states = [State(
-      pre.acc_vector[i], 
-      pre.vel_vector[i], 
-      None) 
-      for i in range(observe_frames, observe_frames+predict_frames)]
-
-  ego_state = State(
-      ego.acc_vector[observe_frames-1], 
-      ego.vel_vector[observe_frames-1], 
-      ego.space_headway_vector[observe_frames-1])
-
-  ds_record = []
-  dt = .1
-  for pre_state in pre_states:
+  def control_law(ego_state, pre_state):
     dv = pre_state.v - ego_state.v
-    da = pre_state.a - ego_state.a
+    gt = ego_state.ds - pre.vehicle_length # pre.vehicle_length is constant
+    return kv * dv + kg * (gt - gs)
+  
+  return simulate(
+      ego.state(observe_frames-1),
+      pre.states(observe_frames, predict_frames),
+      control_law)
 
-    ego_state.ds += dv * dt + .5 * da * dt**2
-    ego_state.v += (ego_state.a) * dt
 
-    gt = ego_state.ds - pre.vehicle_length
-    ego_state.a = kv * dv + kg * (gt - gs) # different for different models
-    ds_record.append(ego_state.ds)
-
-  print(ds_record)
-  print(ego.space_headway_vector[observe_frames:observe_frames+predict_frames])
+def model_free_simulation(
+    ego: Vehicle,
+    pre: Vehicle,
+    observe_frames: int, 
+    predict_frames: int,
+) -> np.ndarray:
+  pass
 
 
 def predict(
     ego: Vehicle, 
-    pre: Vehicle, 
-    observe_frames: int, 
+    pre: Vehicle,
+    observe_frames: int,
     predict_frames: int,
     model='adapt'
 ) -> np.ndarray:
   assert model in ('IDM', 'adapt'), "model should be IDM or adapt"
-  return IDM_simulation(ego, pre) if model is 'IDM' \
-  else adapt_simulation(ego, pre)
+  return IDM_simulation(ego, pre, observe_frames, predict_frames) if model is 'IDM' \
+  else adapt_simulation(ego, pre, observe_frames, predict_frames)
 
 
 if __name__ == '__main__': 
@@ -135,10 +106,14 @@ if __name__ == '__main__':
     l = json.load(fp)
 
   # 3, fail on 1e-4
-  ego = Vehicle(**l[2]['ego'])
-  pre = Vehicle(**l[2]['pre'])
+  ego = Vehicle(**l[11]['ego'])
+  pre = Vehicle(**l[11]['pre'])
 
   # print(ego.vel_vector)
   # print(pre.vel_vector)
 
-  adapt_simulation(ego, pre, 30, 18)
+  observe_frames = 30
+  predict_frames = 18
+  ds_record = adapt_simulation(ego, pre, observe_frames, predict_frames)
+  print(ds_record)
+  print(ego.space_headway_vector[observe_frames:observe_frames+predict_frames])
--- pretraj/vehicle.py
+++ pretraj/vehicle.py
@@ -3,6 +3,10 @@ import numpy as np
 
 # Vehicle = namedtuple('Vehicle', ['vehicle_id', 'frame_id', 'vehicle_length', 'acc_vector', 'vel_vector', 'space_headway_vector'])
 
+class State(object):
+  def __init__(self, a, v, ds):
+    self.a = a; self.v = v; self.ds = ds
+
 class Vehicle(object):
   def __init__(self, vehicle_id, frame_id, vehicle_length, acc_vector, vel_vector, space_headway_vector):
     self.vehicle_id = vehicle_id
@@ -11,3 +15,13 @@ class Vehicle(object):
     self.acc_vector=np.array(acc_vector)
     self.vel_vector=np.array(vel_vector)
     self.space_headway_vector=np.array(space_headway_vector)
+
+  def state(self, i):
+    return State(
+        self.acc_vector[i],
+        self.vel_vector[i],
+        self.space_headway_vector[i])
+
+  def states(self, observe_frames, predict_frames):
+    return [self.state(i)
+        for i in range(observe_frames, observe_frames+predict_frames)]
